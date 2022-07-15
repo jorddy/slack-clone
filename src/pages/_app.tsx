ec0c0be78918ae33f6ae6f0c2987fbe34b96cc3a
Jake <jake.ord345@gmail.com>
2022-07-15 13:17:23 +0100

    updated session provider and env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,27 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { trpc } from "@/utils/trpc";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
 import Header from "@/components/header";
 import Sidebar from "@/components/sidebar";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <div className='h-screen overflow-hidden'>
-      <Head>
-        <title>Slack 2.0 Clone</title>
-      </Head>
-      <Header />
-      <main className='grid grid-cols-5 h-full'>
-        <Sidebar />
-        <Component {...pageProps} />
-      </main>
-      <ReactQueryDevtools />
-    </div>
+    <SessionProvider session={pageProps.session}>
+      <div className='h-screen overflow-hidden'>
+        <Head>
+          <title>Slack 2.0 Clone</title>
+        </Head>
+        <Header />
+        <main className='grid grid-cols-5 h-full'>
+          <Sidebar />
+          <Component {...pageProps} />
+        </main>
+        <ReactQueryDevtools />
+      </div>
+    </SessionProvider>
   );
-}
+};
 
 export default trpc.withTRPC(MyApp);
